dbe5b745a780b1c1432167621227db82e7a9098f
Isaac Carruthers <isaac.carruther@gmail.com>
2016-01-19 22:59:56 -0500

    Use Mutex instead of RwLock in client map, so we can Send server states

--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 use std::sync::mpsc;
-use std::sync::{Arc, RwLock};
+use std::sync::{Arc, RwLock, Mutex};
 use std::sync::atomic::{AtomicUsize, Ordering};
 
 use mio::*;
@@ -13,7 +13,7 @@ pub const SERVER_TOKEN: Token = Token(0);
 
 #[derive(Clone)]
 pub struct WebSocketServerState {
-    clients: Arc<RwLock<HashMap<Token, WebSocketClient>>>,
+    clients: Arc<Mutex<HashMap<Token, WebSocketClient>>>,
     token_counter: Arc<AtomicUsize>
 }
 
@@ -21,7 +21,7 @@ impl WebSocketServerState {
     pub fn new() -> WebSocketServerState {
         WebSocketServerState {
             token_counter: Arc::new(AtomicUsize::new(1)),
-            clients: Arc::new(RwLock::new(HashMap::new()))
+            clients: Arc::new(Mutex::new(HashMap::new()))
         }
     }
 
@@ -29,7 +29,7 @@ impl WebSocketServerState {
                   event_loop_tx: Sender<WebSocketEvent>) -> Token {
         let new_token = Token(self.token_counter.fetch_add(1, Ordering::SeqCst));
 
-        if let Ok(ref mut clients) = self.clients.write() {
+        if let Ok(ref mut clients) = self.clients.lock() {
             clients.insert(new_token, WebSocketClient::new(client_socket, new_token, tx.clone(), event_loop_tx));
         }
 
@@ -37,7 +37,7 @@ impl WebSocketServerState {
     }
 
     pub fn get_peers(&self) -> Option<Vec<Token>> {
-        if let Ok(clients) = self.clients.read() {
+        if let Ok(clients) = self.clients.lock() {
             Some(clients.keys().cloned().collect::<Vec<_>>())
         } else {
             None
@@ -45,7 +45,7 @@ impl WebSocketServerState {
     }
 
     fn remove_client(&mut self, tkn: &Token) -> Option<WebSocketClient> {
-        if let Ok(ref mut clients) = self.clients.write() {
+        if let Ok(ref mut clients) = self.clients.lock() {
             clients.remove(tkn)
         } else {
             None
@@ -59,7 +59,7 @@ impl WebSocketServerState {
             WebSocketEvent::Connect(tkn) |
             WebSocketEvent::Close(tkn) |
             WebSocketEvent::Ping(tkn, _) => {
-                if let Ok(ref mut clients) = self.clients.write() {
+                if let Ok(ref mut clients) = self.clients.lock() {
                     let client = clients.get_mut(&tkn).unwrap();
 
                     client.send_message(msg);
@@ -93,7 +93,7 @@ impl Handler for WebSocketServer {
     fn notify(&mut self, event_loop: &mut EventLoop<WebSocketServer>, msg: WebSocketEvent) {
         match msg {
             WebSocketEvent::Reregister(tkn) => {
-                if let Ok(ref clients) = self.state.clients.read() {
+                if let Ok(ref clients) = self.state.clients.lock() {
                     let client = clients.get(&tkn).unwrap();
                     event_loop.reregister(&client.socket, tkn, client.interest,
                                           PollOpt::edge() | PollOpt::oneshot()).unwrap();
@@ -118,14 +118,14 @@ impl Handler for WebSocketServer {
 
                     let new_token = self.state.new_client(client_socket, self.tx.clone(), event_loop.channel());
 
-                    if let Ok(ref clients) = self.state.clients.read() {
+                    if let Ok(ref clients) = self.state.clients.lock() {
                         event_loop.register(&clients[&new_token].socket,
                                             new_token, EventSet::readable(),
                                             PollOpt::edge() | PollOpt::oneshot()).unwrap();
                     }
                 },
 	        token => {
-                    if let Ok(ref mut clients) = self.state.clients.write() {
+                    if let Ok(ref mut clients) = self.state.clients.lock() {
                         let mut client = clients.get_mut(&token).unwrap();
 
                         client.read();
@@ -138,7 +138,7 @@ impl Handler for WebSocketServer {
         }
 
         if events.is_writable() {
-            if let Ok(ref mut clients) = self.state.clients.write() {
+            if let Ok(ref mut clients) = self.state.clients.lock() {
                 let mut client = clients.get_mut(&token).unwrap();
 
                 client.write();
